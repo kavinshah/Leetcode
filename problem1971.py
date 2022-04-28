a2de85d1a53dcef7988716ab266998b6d50ecf9f
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-04-28 00:46:22 -0400

    python bfs using deque

--- problem1971.py
+++ problem1971.py
@@ -11,37 +11,31 @@ Go figure
 
 
 """
-from collections import defaultdict
+from collections import defaultdict, deque
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
-        def DFS(vertex):
-            nonlocal visited, destination, adj
-            
-            if vertex==destination:
-                return True
-            
-            for neighbour in adj[vertex]:
-                if neighbour not in visited:
-                    visited.add(neighbour)
-                    found=DFS(neighbour)
-                    if found:
-                        return True
-                    
-            return False
-            
-        
         #build adj list
         if source==destination:
             return True
         adj=defaultdict(set)
         visited=set()
-        
         for e in edges:
             adj[e[0]].add(e[1])
             adj[e[1]].add(e[0])
-        
         #print(adj)
         
-        #perform DFS
-        return DFS(source)
+        #perform BFS
+        queue=deque()
+        queue.append(source)
+        visited=set()
+        visited.add(source)
+        while(queue):
+            current=queue.popleft()
+            for neighbour in adj[current]:
+                if neighbour==destination:
+                    return True
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.append(neighbour)
+                    
+        return False
