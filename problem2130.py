7b9e1568d08946aaab34cfb09f3b6e7bd3357796
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-05-04 21:38:12 -0400

    fast slow pointer and linear time and space effecient

--- problem2130.py
+++ problem2130.py
@@ -16,29 +16,40 @@ reverse the list from n/2+1 to n and then run 2 pointers starting at 0 and n/2+1
 approach 3:
 create another list in reverse order and then evaluate the pairs from 0 to n/2-1
 
+5->4->2->1
+
+5->4
+
+
+
 """
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        count = 0
-        curr = head
-        newhead = None
-        # get the length of linked list
-        while(curr):
-            node = ListNode(curr.val)
-            node.next = newhead
-            newhead = node
-            
-            count+=1
-            curr=curr.next
-            
+        
+        def reverseList(node):
+            nextnode=None
+            curr=node
+            prev=None
+            while(curr):
+                nextnode=curr.next
+                curr.next=prev
+                prev=curr
+                curr=nextnode
+
+            return prev
+        
+        slow = head
+        fast = head
+        
+        while(fast):
+            slow=slow.next
+            fast=fast.next.next
+         
+        mid = reverseList(slow)
         maxsum=float('-inf')
-        curr=head
-        newcurr = newhead
-        for i in range(count//2):
-            maxsum = max(maxsum, curr.val+newcurr.val)
-            newcurr=newcurr.next
-            curr=curr.next
-            
-        print(maxsum)
+        while(mid and head):
+            maxsum=max(maxsum, mid.val+head.val)
+            mid=mid.next
+            head=head.next
         
         return maxsum
