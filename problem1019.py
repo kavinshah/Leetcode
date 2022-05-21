629fae243c6d148fe3a9c5b12c4bb3fc2c00b781
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-05-21 15:35:50 -0400

    left to right using stack. space efficient. good approach

--- problem1019.py
+++ problem1019.py
@@ -4,30 +4,26 @@
 #         self.val = val
 #         self.next = next
 """
+approach 1:
 [5,3] 
 pop from stack until stack is not empty and curr element > stack.top
 use stack.top else 0
 push current element on stack.
 
+[2,1,5]
+approach 2:
+stack = [[0,2]], res = [0]
+stack = [[0,2],[1,1]], res = [0,0]
+stack = [], res = [5,5,0]
 
 """
 class Solution:
-    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-        result=deque()
-        stack = []
-        def traverseNodes(node):
-            nonlocal result, stack
-            if node.next:
-                traverseNodes(node.next)
-            while(stack and node.val>=stack[-1]):
-                stack.pop()
-            if stack:
-                result.appendleft(stack[-1])
-            else:
-                result.appendleft(0)
-            stack.append(node.val)
-            return
-        
-        traverseNodes(head)
-        return result
-                
+    def nextLargerNodes(self, head):
+        res, stack = [], []
+        while head:
+            while stack and stack[-1][1] < head.val:
+                res[stack.pop()[0]] = head.val
+            stack.append([len(res), head.val])
+            res.append(0)
+            head = head.next
+        return res
