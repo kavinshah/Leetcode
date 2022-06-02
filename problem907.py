d78366ac0acae411f17f301157189fc8156616f6
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-06-02 02:51:21 -0400

    leetcode explanations solution

--- problem907.py
+++ problem907.py
@@ -15,37 +15,37 @@ result = 3+2+4+8=17
 
 [3 1 2 6 5 2 4]
 
+explanation of sums:
+
+the minimum of subarrays which has the current element will be:
+total number of subarrays with current element as minimum [X] =(position of minimum element on left side of current element * position of minimum element on right side of currrent element)
+total sum of such subarrays = current element * X
 
 """
 class Solution:
-    def sumSubarrayMins(self, arr):
-        
-        prev_lesser = []   # optimization for finding minimum
-        sum_of_minima_up_to = [0] * len(arr)
-        
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        left=[i+1 for i in range(len(arr))]
+        right=[len(arr)-i for i in range(len(arr))]
+        stack = []
         for i in range(len(arr)):
+            #get the prev less element
+            while(stack and arr[stack[-1]]>arr[i]):
+                x=stack.pop()
+                # use the element popped from stack and set value in right array.
+                # This is because all elements in the stack greater than current element
+                # must have current element as lesser on the right.
+                right[x]=i-x 
             
-            # find j: "closest" element that is smaller than arr[i]
-            while len(prev_lesser) > 0 and arr[prev_lesser[-1]] > arr[i]:
-                prev_lesser.pop()
-            
-            if len(prev_lesser) == 0:
-                # arr[i] is lesser than anything to its left.
-                # hence, all sub-arrays up to index i will have arr[i] as their minimum.
-                # there are i+1 such sub-arrays, hence the sum is (i+1)*arr[i].
-                sum_of_minima_up_to[i] = (i+1) * arr[i]
+            # set the left array for current element with topmost element in stack since this 
+            # is the element which is smaller than current element
+            if stack:
+                left[i]=i-(stack[-1])
             else:
-                # for some j (top of stack) we have arr[j] is less than arr[i].
-                # all the sub-arrays that include j will have the same minimum sum as sum_of_minima_up_to[j].
-                # all of the sub-arrays that do not include j (to the "right" of j) will have arr[i] as their
-                # minimum, and there are (i-j) such arrays.
-                j = prev_lesser[-1]
-                sum_of_minima_up_to[i] = sum_of_minima_up_to[j] + (i-j) * arr[i]
+                left[i]=i+1
+            stack.append(i)
             
-            # stack remains non-decreasing since we popped all the larger elements on lines 10-11
-            prev_lesser.append(i)
+        return sum(arr[i]*left[i]*right[i] for i in range(len(arr))) % 1000000007
         
-        return sum(sum_of_minima_up_to) % (10**9 + 7)
         
 #Time: O(N)
 #Space: O(N)
