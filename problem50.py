606ad02080eb036f74cca61e6a4527a57dc415b9
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-06-22 19:03:43 -0400

    iterative bottom up

--- problem50.py
+++ problem50.py
@@ -1,24 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        cache = {0:1}
-        
-        def calculatePow(n):
-            nonlocal cache, x
-            
-            if n in cache:
-                return cache[n]
-            
-            if not n%2:
-                cache[n] = calculatePow(n/2) * calculatePow(n/2)
-            else:
-                cache[n] = calculatePow(n//2) * calculatePow(n//2) * x
-        
-            return cache[n]
-        
-        cache[1]=x
-        if n>0:
-            calculatePow(n)
-            return cache[n]
-        else:
-            calculatePow(-n)
-            return 1/cache[-n]
+        if n<0:
+            x = 1/x
+            n=-n
+        ans = 1
+        prod = x
+        while(n>0):
+            if n%2==1:
+                ans=ans*prod
+            prod=prod*prod
+            n=n//2
+        return ans
