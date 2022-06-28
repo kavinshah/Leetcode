d0e3b8e2aebd5a6116fec017e3cade69bbb2f9d0
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-06-28 03:48:06 -0400

    product optimized

--- problem238.py
+++ problem238.py
@@ -1,20 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1]*len(nums)
-        right = [1]*len(nums)
-        
         prod = [1]*len(nums)
-        
         for i in range(1, len(nums)):
-            left[i]=left[i-1]*nums[i-1]
+            prod[i]=prod[i-1]*nums[i-1]
+        R=1 
+        for i in range(len(nums)-1, -1, -1):
+            prod[i]=prod[i]*R
+            R=R*nums[i]
             
-        for i in range(len(nums)-2, -1, -1):
-            right[i]=right[i+1]*nums[i+1]
             
-        for i in range(len(nums)):
-            prod[i]=left[i]*right[i]
-            
-        # print(left)
-        # print(right)
-        # print(prod)
         return prod
+        
+#Time: O(N)
+#Space: O(1)
