f96bd01e1239b444d8b05ecb46d5e7dadec4f1db
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-08 09:52:28 -0400

    bottom up iteration

--- problem70.py
+++ problem70.py
@@ -9,13 +9,10 @@
 """
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memoize = {0:1, 1:1}
-        def climb(x):
-            if x in memoize:
-                return memoize[x]
-            if x<0:
-                return 0
-            memoize[x] = climb(x-1) + climb(x-2)
-            return memoize[x]
-        
-        return climb(n)
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2, n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+            
+        return dp[n]
