8a4f9a8c5a9203d068ff86a5ed79809c0453f31c
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-08 09:51:51 -0400

    top down recursion

--- problem70.py
+++ problem70.py
@@ -1,45 +1,21 @@
 """
+                                0
+                            1       2
+                          2   3    3   4
+                          
 
-                    5
-            /               \
-           5-1=4            5-2
-        /       \
-        3        2
-      /   \
-      2    1
-  /     \  /\
-   1    0  0 -1
-  / \   |  |  |
-  0  -1 +1 +1 0
-  |   |
-  +1  0
-
-0:0
-1:1
-2:2
-3:3
-4:5
-5:8
 
 
 """
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def findPath(n):
-            nonlocal ways
-            if n==0:
-                return 1
-            
-            if n<0:
+        memoize = {0:1, 1:1}
+        def climb(x):
+            if x in memoize:
+                return memoize[x]
+            if x<0:
                 return 0
-            
-            if n in ways:
-                return ways[n]
-            
-            ways[n]=findPath(n-1) + findPath(n-2)
-            return ways[n]
+            memoize[x] = climb(x-1) + climb(x-2)
+            return memoize[x]
         
-        count=0
-        ways = {0:0, 1:1}
-        
-        return findPath(n)
+        return climb(n)
