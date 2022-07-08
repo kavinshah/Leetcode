9fe79534577fa13f0bc6e30a7aed151f3761a181
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-08 12:15:19 -0400

    bottom up iteration

--- problem322.py
+++ problem322.py
@@ -1,37 +1,23 @@
 """
-
-                            11
-                    /       |       \
-                   6        9       10
-                /   |   \
-              1     4   5
-             /|\
-           -5-1 0
+bottom up approach iteration:
+dp[i]= min(dp[i], 1+dp[i-c]) - c=coins, and i-c>=0
+     = -1                     - otherwise
 
 """
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins=sorted(coins, reverse=True)
-        memoization = [0]*(amount+1)
-        def performChange(x):
-            nonlocal coins, memoization
-            if x==0:
-                return 0
-            if x<0:
-                return -1
-            if memoization[x]!=0:
-                return memoization[x]
-            minimum = float('inf')
-            for c in coins:
-                result = performChange(x-c)
-                if result>=0:
-                    minimum = min(minimum, 1+result)
-            if minimum == float('inf'):
-                memoization[x]=-1
-            else:
-                memoization[x]=minimum
-            return memoization[x]
         
-        result=performChange(amount)
-        return result
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for i in range(amount+1):
+            for c in coins:
+                if (i-c)>=0:
+                    dp[i]=min(dp[i], 1+dp[i-c])
+                    
+        if dp[amount]==float('inf'):
+            return -1
+        return dp[amount]
+    
+    
+#time: O(amount*N)
+#space: O(amount)
