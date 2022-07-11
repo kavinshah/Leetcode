52b1bbb1458b22db3ee4550b5637b551a75a8dc8
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-11 18:47:08 -0400

    top down iterative

--- problem198.py
+++ problem198.py
@@ -50,17 +50,13 @@ memo=[2,2,3,4,10,13,13,14], i=7
 """
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp={}
-        def findMaxAtHouse(index):
-            nonlocal dp, nums
-            if index>=len(nums):
-                return 0
-            if index in dp:
-                return dp[index]
-            dp[index]=max(findMaxAtHouse(index+2)+nums[index], findMaxAtHouse(index+1))
-            return dp[index]
-        
-        return findMaxAtHouse(0)
+        dp=[0]*(len(nums)+1)
+        dp[-1]=0
+        dp[-2]=nums[-1]
+        for i in range(len(nums)-2, -1,-1):
+            dp[i]=max(dp[i+1], nums[i]+dp[i+2])
+            
+        return dp[0]
     
 #Time: O(N)
 #Space: O(N)
