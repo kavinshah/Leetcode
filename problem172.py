d7bf9af18150d3a158739c533cdaea9394f29bed
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-12 22:53:58 -0400

    log time solution

--- problem172.py
+++ problem172.py
@@ -32,22 +32,9 @@ factortwo=26
 """
 
 class Solution:
-    def trailingZeroes(self, n: int) -> int:
-        # powerfive = {5:1,25:2,125:3,625:4,3125:5}
-        # powertwo = {2:1, 4:2,8:3,16:4,32:5,64:6,128:7,256:8,512:9,1024:10,2048:11,4096:12,8192:13}
-        #powerten = {10:1,100:2,1000:3,10000:4,100000:5}
-        factorten=0
+    def trailingZeroes(self, i: int) -> int:
         factorfive=0
-        factortwo=0
-        for counter in range(1,n+1):
-            i=counter
-            while(i%10==0):
-                factorten+=1
-                i=i//10
-                
-            while(i%5==0):
-                factorfive+=1
-                i=i//5
-        #print(factorten, factorfive, factortwo)
-        return factorten+factorfive
-        
+        while(i!=0):
+            i=i//5
+            factorfive+=i
+        return factorfive
