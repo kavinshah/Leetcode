65259a14bdd59dfba102f141da584a6a8f12e718
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-22 12:38:33 -0400

    using rolling hash

--- problem1461.py
+++ problem1461.py
@@ -1,12 +1,13 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        count = 1<<k
-        visited = set()
-        current = []
-        for i in range(k, len(s)+1):
-            if s[i-k:i] not in visited:
-                visited.add(s[i-k:i])
-                count-=1
-        return count==0
-            
-        
+        need = 1<<k
+        all_ones = (1<<k) - 1
+        counts = [0]*need
+        hashval=0
+        for i in range(len(s)):
+            hashval = (hashval<<1 & all_ones) | int(s[i])
+            if i>=(k-1) and counts[hashval]==0:
+                counts[hashval]=1
+                need-=1
+                
+        return need==0
