c8b1a4b5f511da0c3cf33e782c8eac93855032a3
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-21 22:22:25 -0400

    using set approaach

--- problem1461.py
+++ problem1461.py
@@ -2,13 +2,11 @@ class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         count = 1<<k
         visited = set()
-        current = deque(s[:(k-1)])
-        for i in range(k-1, len(s)):
-            current.append(s[i])
-            if ''.join(current) not in visited:
-                visited.add(''.join(current))
+        current = []
+        for i in range(k, len(s)+1):
+            if s[i-k:i] not in visited:
+                visited.add(s[i-k:i])
                 count-=1
-            current.popleft()
         return count==0
             
         
