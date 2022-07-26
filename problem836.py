a10962ab56a574d5b7075c0cde0805713eb92253
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-25 20:49:27 -0400

    leetcode solution

--- problem836.py
+++ problem836.py
@@ -36,35 +36,11 @@
 
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
-        if rec1[0]<=rec2[0]:
-            small_x1 = rec1[0]
-            small_y1 = rec1[1]
-            small_x2 = rec1[2]
-            small_y2 = rec1[3]
-            large_x1 = rec2[0]
-            large_y1 = rec2[1]
-            large_x2 = rec2[2]
-            large_y2 = rec2[3]
-        else:
-            small_x1 = rec2[0]
-            small_y1 = rec2[1]
-            small_x2 = rec2[2]
-            small_y2 = rec2[3]
-            large_x1 = rec1[0]
-            large_y1 = rec1[1]
-            large_x2 = rec1[2]
-            large_y2 = rec1[3]
-            
-        # the smaller rectange is to the left or larger rectange.
-        #check if the large rectangle is above or below or right of smaller rectangle.
-        if small_x1 < large_x1:
-            if large_y1 >= small_y2 or large_y2 <= small_y1 or large_x1 >= small_x2:
+        #if either rectangle is a line, return False
+        if rec1[0]==rec1[2] or rec1[1]==rec1[3] \
+            or rec2[0]==rec2[2] or rec2[1]==rec2[3]:
                 return False
-            return True
             
-        # if they are on small x-axis, then only check if larger rectange is above the smaller rectange since it cannot be below the smaller rectangle.
-        if small_x1 == large_x1:
-            if large_y1>= small_y2:
-                return False
-            return True
-            
+        return not (rec1[2] <= rec2[0] or rec1[0] >= rec2[2] or rec1[1] >= rec2[3] or rec1[3] <= rec2[1])
+        
+        
