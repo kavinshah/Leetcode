6bb246a4de80d6563e9b197cbd00c6507dd0c4a1
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-07-30 18:53:55 -0400

    constant space approach

---
+++ problem143.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+#     1 -> 2 -> 3
+# odd= slow.next to end
+
+#     1 -> 2 -> 3 -> 4
+    
+# even = prev.next to end
+    
+#     1->2->3->4->5
+# odd=prev.next to end
+
+#     1->2->3->4->5->6
+
+# 1->2->null
+# 3->null
+
+# 1->3->2
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head==None or head.next==None:
+            return head
+        
+        slow=head
+        fast=head
+        prev=None
+        while(fast and slow):
+            fast=fast.next
+            if not fast:
+                prev=prev.next
+                break
+            fast=fast.next
+            prev=slow
+            slow=slow.next
+            
+        head2=prev.next
+        prev.next=None
+        
+        #reverse from head2 to end
+        current=head2
+        nextNode=None
+        prev=None
+        while current:
+            nextNode=current.next
+            current.next=prev
+            prev=current
+            current=nextNode
+        
+        head2=prev
+        
+        #merge 2 lists
+        h1=head
+        h2=head2
+        #print(head,head2)
+        while h1 and h2:
+            t2=h2
+            h2=h2.next
+            t1=h1.next
+            h1.next=t2
+            t2.next=t1
+            h1=t1
+            
+        return head
+
+
+        
