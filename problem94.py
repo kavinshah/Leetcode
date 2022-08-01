b50287fd9c2bcfbad254057cefc0428eee173235
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-08-01 08:39:21 -0400

    python solutions

---
+++ problem94.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+
+                1
+             /      \
+            2        3
+         /    \     /
+        4     5    6
+stack=[1]
+left=[2], result=[1]
+left=[4], stack = [], right=[3], result=[2,1]
+left=[], stack = [], right=[3,5], result=[4, 2,1]
+left=[], stack = [], right=[3], result=[4, 2,1,5]
+left=[], stack = [], right=[3], result=[4, 2,1,5]
+
+
+left + root.val + right
+stack=[], right=[], result=[1]
+stack=[], right=[], result=[1]
+
+"""
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result=[]
+        stack = [root]
+        while(stack):
+            
