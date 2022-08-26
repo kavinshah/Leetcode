de9576098a9a1f88f6b0ae0097d208859021926a
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-08-25 20:49:26 -0400

    linear approach

--- problem54.py
+++ problem54.py
@@ -1,59 +1,56 @@
 """
 
-1   2   3   4   21
-5   6   7   8   22
-9   10  11  12  23
-13  14  15  16  24
-25  26  27  28  29
+right: cmin -> cmax, rmin=rmin+1
+down : rmin -> rmax, cmax=cmax-1
+left : cmax -> cmin, rmax=rmax-1
+up   : rmax -> rmin, cmin=cmin+1
 
+until rmax>=rmin and cmax>=cmin
 
-1. lowC, highC=0,4
-   lowR, highR = 0,4
-2. while lowC<=highC and lowR<=highR:
-    if 
+[1, 2, 3, 4]
+[5, 6, 7, 8]
+[9,10,11,12]
+
 
 """
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
-        lowR, highR = 0, len(matrix)-1
-        lowC, highC = 0, len(matrix[0])-1
-        result = []
-        curr_direction = directions[0]
-        currR, currC=0, -1
-        i=0
-        while lowC<=highC and lowR<=highR:
-            if curr_direction == (0,1):
-                currC+=1
-            elif curr_direction == (1,0):
-                currR+=1
-            elif curr_direction == (0,-1):
-                currC-=1
-            else:
-                currR-=1
-            if lowR>currR or currR>highR or lowC>currC or currC>highC:
-                #change direction
-                if curr_direction == (0,1):
-                    currR=lowR
-                    currC=highC
-                    lowR+=1
-                elif curr_direction == (1,0):
-                    currR=highR
-                    currC=highC
-                    highC-=1
-                elif curr_direction == (0,-1):
-                    currR=highR
-                    currC=lowC
-                    highR-=1
-                else: #curr_direction == (-1,0):
-                    currR=lowR
-                    currC=lowC
-                    lowC+=1
-                i+=1
-                curr_direction=directions[i%4]
-                continue
+        rmin=0
+        rmax=len(matrix)-1
+        cmin=0
+        cmax=len(matrix[0])-1
+        result=[]
+        while(rmax>=rmin and cmax>=cmin):
+            #right
+            for i in range(cmin, cmax+1):
+                result.append(matrix[rmin][i])
+            rmin=rmin+1
+            #print("right", result)
             
-            #print(currR, currC, curr_direction)
-            result.append(matrix[currR][currC])
+            #down
+            for i in range(rmin, rmax+1):
+                result.append(matrix[i][cmax])
+            cmax=cmax-1
+            #print("down", result)
+            
+            #left
+            if rmax>=rmin:
+                for i in range(cmax, cmin-1, -1):
+                    result.append(matrix[rmax][i])
+            rmax=rmax-1
+            #print("left", result)
+            
+            #up
+            if cmax>=cmin:
+                for i in range(rmax, rmin-1, -1):
+                    result.append(matrix[i][cmin])
+            cmin=cmin+1
+            #print("up", result)
+            
+            #print(rmin,rmax,cmin,cmax)
                 
         return result
+        
+#time: O(MN)
+#space: O(1)
