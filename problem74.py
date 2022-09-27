61fcdae8512d350a56102c6a1ebf6dd27fae2e78
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-09-26 20:51:30 -0400

    binary search 1 pass

--- problem74.py
+++ problem74.py
@@ -1,29 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #first find the row
+        rowL=len(matrix)
+        colL=len(matrix[0])
         low=0
-        high=len(matrix)-1
-        mid=-1
+        high=rowL*colL-1
         row=-1
+        col=-1
         while(low<=high):
             mid=(low+high)//2
-            if matrix[mid][0] <= target <= matrix[mid][-1]:
-                break
-            if matrix[mid][0]>target:
-                high=mid-1
-            else:
-                low=mid+1
-                
-        row=mid
-        
-        #find the col
-        low=0
-        high=len(matrix[0])-1
-        while(low<=high):
-            mid=(low+high)//2
-            if matrix[row][mid]==target:
+            row=mid//colL
+            col=mid%colL
+            if matrix[row][col]==target:
                 return True
-            if matrix[row][mid]>target:
+            if matrix[row][col]>target:
                 high=mid-1
             else:
                 low=mid+1
