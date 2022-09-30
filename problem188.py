51c8b4e2f137f4166786c82e9cf8dad8df045a7f
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-09-30 08:58:52 -0400

    solution 3 -memoization from andre

--- problem188.py
+++ problem188.py
@@ -1,9 +1,13 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        min_price = [float("inf")] * (k + 1)
-        max_profit = [0] * (k + 1)
-        
-        for j in range(len(prices)):
-            for i in range(1, k + 1):
-                min_price[i] = min(min_price[i], prices[j] - max_profit[i-1])
-                max_profit[i] = max(max_profit[i], prices[j] - min_price[i])
+        memo = {}
+        def rec(k, i):
+            if i < 0 or k <= 0:
+                return 0
+            if (i, k) in memo:
+                return memo[(i, k)]
+            res = max(rec(k, i-1),  # do not sell that day
+                      max((prices[i] - prices[j] + rec(k-1, j-1) for j in range(i)), default=0))  # sell that day, buy on day j
+            memo[(i, k)] = res
+            return res
+        return rec(k, len(prices)-1)
