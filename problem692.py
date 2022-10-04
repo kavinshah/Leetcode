9312f20fd386180a11e6f4d233faa5ce3e871b96
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-10-04 19:28:02 -0400

    python leetcode solution

--- problem692.py
+++ problem692.py
@@ -12,29 +12,55 @@ space: O(N)
 
 """
 
+"""
+
+1. keep a hashmap of all the words and their counts
+2. maintain a min heap of size k based on their counts and insert the words
+3. return everything in the minheap
+
+time: O(Nlogk) -- N=total size, M=no. of unique elements, K=input k
+space: O(N)
+
+"""
+
+from collections import Counter
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        counts = defaultdict(int)
-        for w in words:
-            counts[w]+=1
-        minheap = []
-        result=deque()
-        for w in counts:
-            if len(minheap)<k:
-                heappush(minheap, [counts[w], w])
-            else:
-                print(w, minheap)
-                if (minheap[0][0] == counts[w] and minheap[0][1] >= w) or minheap[0][0] < counts[w]:
-                    heappop(minheap)
-                    heappush(minheap, [counts[w], w])
+        n = len(words)
+        cnt = Counter(words)
+        bucket = [{} for _ in range(n+1)]
+        self.k = k
 
-        while(minheap):
-            curr=heappop(minheap)
-            stack = []
-            while(result and result[0][0] == curr[0] and result[0][1]<curr[1]):
-                stack.append(result.popleft())
-            result.appendleft(curr)
-            while(stack):
-                result.appendleft(stack.pop())
-        #print(result)
-        return [r[1] for r in result]
+        def add_word(trie: Mapping, word: str) -> None:
+            root = trie
+            for c in word:
+                if c not in root:
+                    root[c] = {}
+                root = root[c]
+            root['#'] = {}
+
+        def get_words(trie: Mapping, prefix: str) -> List[str]:
+            if self.k == 0:
+                return []
+            res = []
+            if '#' in trie:
+                self.k -= 1
+                res.append(prefix)
+            for i in range(26):
+                c = chr(ord('a') + i)
+                if c in trie:
+                    res += get_words(trie[c], prefix+c)
+            return res
+
+        for word, freq in cnt.items():
+            add_word(bucket[freq], word)
+
+        res = []
+        for i in range(n, 0, -1):
+            if self.k == 0:
+                return res
+            if bucket[i]:
+                res += get_words(bucket[i], '')
+        return res
