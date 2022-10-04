8fb10710abf7c5872622f84beec4549108600ad5
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-10-04 19:15:58 -0400

    using heaps

---
+++ problem378.py
@@ -0,0 +1,23 @@
+"""
+
+[1, 5,  9]
+[10,11,13]
+[12,13,15]
+
+
+
+"""
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        maxheap=[]
+        N=len(matrix)*len(matrix)
+        for r in matrix:
+            for x in r:
+                if len(maxheap)<(N-k+1):
+                    heappush(maxheap, x)
+                elif maxheap[0] < x:
+                    heappop(maxheap)
+                    heappush(maxheap, x)
+
+        return maxheap[0]
