682a49643f05d0175e4be72ce22463ba25535738
kavinshah <38910831+kavinshah@users.noreply.github.com>
2022-10-26 18:09:48 -0400

    simple python using int arrays

---
+++ problem36.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [[0]*9 for i in range(9)]
+        cols = [[0]*9 for i in range(9)]
+        boxes = [[[0]*9 for i in range(3)], [[0]*9 for i in range(3)], [[0]*9 for i in range(3)]]
+        print(boxes)
+        for r in range(9):
+            for c in range(9):
+                item = board[r][c]
+                if item==".":
+                    continue
+                item = int(item) - 1
+                if rows[r][item]==1:
+                    return False
+                if cols[c][item]==1:
+                    return False
+                if boxes[r//3][c//3][item]==1:
+                    return False
+
+                rows[r][item]=1
+                cols[c][item]=1
+                boxes[r//3][c//3][item]=1
+                
+        return True
+
