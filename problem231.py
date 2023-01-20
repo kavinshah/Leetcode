1f702abcf9cb640f8cd35096f2465572ad3eeba4
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-01-19 19:26:22 -0500

    adding pseudo code

--- problem231.py
+++ problem231.py
@@ -5,6 +5,24 @@
 0111
 1000
 
+
+n=7
+
+n            = 0000 0111
+~n           = 1111 1000
+~n+1 i.e. -n = 1111 1001
+n&(-n)       = 0000 0001
+
+since n&-n != n, return False
+
+n=8
+
+n           = 0000 1000
+-n          = 1111 1000
+n&-n        = 0000 1000
+
+since n==n&-n, return True
+
 """
 
 class Solution:
