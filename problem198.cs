e5fb8d6ed23db1b39a85f4b4746b61305cd43921
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-02-01 20:47:31 -0500

    problem198.cs

---
+++ problem198.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    Dictionary<int, int> memoization = new Dictionary<int, int>();
+    int[] nums;
+    public int Rob(int[] nums) {
+        this.nums = nums;
+        return helper(0);
+    }
+    
+    public int helper(int index)
+    {
+        if(index>=nums.Length)
+        {
+            return 0;
+        }
+        
+        if(memoization.ContainsKey(index))
+        {
+            return memoization[index];
+        }
+        
+        memoization[index] = Math.Max(nums[index]+helper(index+2), helper(index+1));
+        return memoization[index];
+    }
+}
+
+#time: O(N)
+#Space: O(N)
