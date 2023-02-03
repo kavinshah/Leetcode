051dd7dba5d003c27dd86913941259481d535f2a
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-02-03 15:27:10 -0500

    optimized approach

--- problem567.cs
+++ problem567.cs
@@ -1,20 +1,24 @@
 public class Solution {
     public bool CheckInclusion(string s1, string s2) {
         int[] items1 = new int[26];
-        int[] items2;
+        int[] items2 = new int[26];
         int currentIndex = 0;
         if(s1.Length > s2.Length){
             return false;
         }
         for(int i=0;i<s1.Length;i++){
             items1[s1[i]-'a'] += 1;
+            items2[s2[i]-'a'] += 1;
         }
+        
+        if(Compare(items1, items2))
+            return true;
+        
+        currentIndex = s1.Length;
         while(currentIndex < s2.Length){
-            items2=new int[26];
-            int end = Math.Min(s2.Length, currentIndex+s1.Length);
-            for(int i=currentIndex; i<end; i++){
-                items2[s2[i]-'a'] += 1;
-            }
+            items2[s2[currentIndex-s1.Length]-'a'] -= 1;
+            items2[s2[currentIndex]-'a'] += 1;
+            
             if (Compare(items1, items2)){
                 return true;
             }
