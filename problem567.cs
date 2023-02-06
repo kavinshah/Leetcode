4dde711b1dbd1614638e9889555e76b052fb3984
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-02-06 09:26:59 -0500

    csharp more time efficient solution

--- problem567.cs
+++ problem567.cs
@@ -3,6 +3,8 @@ public class Solution {
         int[] items1 = new int[26];
         int[] items2 = new int[26];
         int currentIndex = 0;
+        int matchCount = 0;
+        
         if(s1.Length > s2.Length){
             return false;
         }
@@ -11,32 +13,42 @@ public class Solution {
             items2[s2[i]-'a'] += 1;
         }
         
-        if(Compare(items1, items2))
-            return true;
+        for(int i=0; i<26; i++){
+            if (items1[i]==items2[i]){
+                matchCount++;
+            }
+        }
         
         currentIndex = s1.Length;
         while(currentIndex < s2.Length){
-            items2[s2[currentIndex-s1.Length]-'a'] -= 1;
-            items2[s2[currentIndex]-'a'] += 1;
-            
-            if (Compare(items1, items2)){
+            if(matchCount==26){
+                Console.WriteLine("{0}, {1}", matchCount, currentIndex);
+                Console.WriteLine(String.Join(",", items2));
                 return true;
+            }
+            items2[s2[currentIndex]-'a'] += 1;
+            if(items2[s2[currentIndex]-'a'] == items1[s2[currentIndex]-'a'])
+            {
+                matchCount++;
+            } else if (items2[s2[currentIndex]-'a'] == (items1[s2[currentIndex]-'a'] + 1)) {
+                matchCount--;
+            }
+            
+            items2[s2[currentIndex-s1.Length]-'a'] -= 1;
+            if(items2[s2[currentIndex-s1.Length]-'a'] == items1[s2[currentIndex-s1.Length]-'a'])
+            {
+                matchCount++;
+            } else if (items2[s2[currentIndex-s1.Length]-'a'] == (items1[s2[currentIndex-s1.Length]-'a'] - 1)) {
+                matchCount--;
             }
             currentIndex++;
         }
-        return false;
-    }
-    
-    public bool Compare(int[] x, int[] y)
-    {
-        for(int i =0; i<26;i++){
-            if(x[i]!=y[i]){
-                return false;
-            }
-        }
-        return true;
+        
+        //Console.WriteLine(matchCount);
+        
+        return (matchCount==26);
     }
 }
 
-// time: O(M+N)
-// space: O(M+N)
+// time: O(N)
+// space: O(1)
