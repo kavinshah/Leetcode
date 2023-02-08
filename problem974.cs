9894386109c137dd94169c581aefbd0e917552b2
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-02-07 20:53:26 -0500

    prefix subarray solution

---
+++ problem974.cs
@@ -0,0 +1,41 @@
+/*
+
+1   2   3   2   2   3   5
+    i       j       k   l
+    
+
+[4 5 0 -2 -3 1]
+
+negative numbers and so cannot use sliding window approach
+lets use prefix approach. Not sure how that works though
+
+[4] [4 5]
+
+*/
+
+public class Solution {
+    public int SubarraysDivByK(int[] nums, int k) {
+        int count=0;
+        int currsum=0;
+        Dictionary<int, int> frequency = new Dictionary<int, int>(100000);
+        frequency[0]=1;
+        int mod;
+        foreach(int num in nums){
+            currsum+=num;
+            mod=currsum%k;
+            if(mod<0){
+                mod+=k;
+            }
+            if(!frequency.ContainsKey(mod)){
+                frequency[mod]=0;
+            }
+            count+=frequency[mod];
+            frequency[mod]++;
+        }
+        return count;
+    }
+}
+
+//Time: O(N)
+//Space: O(N)
+    
