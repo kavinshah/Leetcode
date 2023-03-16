c9510ee503bb34e0611cc5d60abfb18ed906b00f
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-03-16 15:13:11 -0400

    using monotonic stack

--- problem84.cs
+++ problem84.cs
@@ -1,29 +1,46 @@
+/*
+
+monotonic stack:-
+-- when you find larger elements on the top of stack, pop the top
+-- the first loop calculates the maximum height based on nearest smaller bars. This loop only calculates the area bounded by stack top and current smallest bar.
+
+-- the second loop keep the end of array fixed and calculates the total area bounded by top of stack and end of array. This is required since we need to find out the area bounded by all the smaller bars on the stack.
+
+
+2 1 2
+
+    ____    ____
+    |   |___|   |
+    |   |   |   |
+------------------------
+
+
+*/
+
+
 public class Solution {
     public int LargestRectangleArea(int[] heights) {
-        Stack<int> stack = new Stack<int>();
-        stack.Push(-1);
         int result=0;
+        Stack<int> stack = new Stack<int>();
+        stack.Push(0);
         
         for(int i=0; i<heights.Length; i++)
         {
-            while(stack.Count>1 && heights[stack.Peek()] > heights[i])
+            
+            while(stack.Count>1 && heights[stack.Peek()]>heights[i])
             {
-                int ht = heights[stack.Pop()];
-                int width = i-stack.Peek()-1;
-                result=Math.Max(result, width*ht);
+                int popped=stack.Pop();
+                result=Math.Max(result, heights[popped]*(i-stack.Peek()-1));
             }
-            stack.Push(i);   
+            stack.Push(i);
         }
         
         while(stack.Count>1)
         {
-            int ht=heights[stack.Pop()];
-            int width=heights.Length-stack.Peek()-1;
-            result=Math.Max(result, ht*width);
-        } 
+            int popped=stack.Pop();
+            result=Math.Max(result, heights[popped]*(heights.Length-stack.Peek()-1));
+        }
+        
         return result;
     }
 }
-
-//time: O(N)
-//space: O(N)
