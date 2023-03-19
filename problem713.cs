3377519b546e07afb53f82636a6f98aa04d39b68
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-03-19 18:38:01 -0400

    linear approach

--- problem713.cs
+++ problem713.cs
@@ -1,41 +1,36 @@
-/*
-
-k=100
-10 5 2 6
-
-10  5   2   6
-
-i=0;currprod=10;count=1;start=0
-i=1;currprod=50;count=3;start=0
-i=2;currprod=10;count=5;start=1
-i=3;currprod=60;count=8;start=1
-
-k=0
-1 2 3
-
-i=0;currprod=1;start=0;count=0
-
-*/
-
 public class Solution {
     public int NumSubarrayProductLessThanK(int[] nums, int k) {
+        int left=0, right=0;
+        long total=1;
+        int result=0;
         
-        if(k==0)
-            return 0;
-        long currprod=1;
-        int count=0;
-        int start=0;
-        for(int i=0; i<nums.Length; i++){
-            currprod*=nums[i];
-            while(currprod>=k && start<=i){
-                currprod=currprod/nums[start++];
+        if(k<=1) return 0;
+        
+        while(right<nums.Length)
+        {
+            if(total<k)
+            {
+                total=total*nums[right];
+                right++;
             }
-            if(start<=i){
-                count+=(i-start+1);
+            else
+            {
+                total=total/nums[left];
+                left++;
             }
-            //Console.WriteLine("i={0},currprod={1},count={2},start={3}",i,currprod,count,start);
+            
+            if(total<k)
+                result += (right-left);   
         }
-        return count;
+        
+        while(total>k && left<nums.Length)
+        {
+            total=total/nums[left];
+            left++;
+            if(total<k)
+                result+=right-left;
+        }
+        return result;
     }
 }
 
