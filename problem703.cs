db380daa41ccb9cb5ad71bb8aa4d02a45180d24e
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-04-03 19:55:10 -0400

    using minheap

--- problem703.cs
+++ problem703.cs
@@ -1,56 +1,21 @@
-/*
-
-1. use a max heap
-2. store each element as its complement. so that max heap behaves as a min heap with k largest elements in the heap.
-3. the top of the heap stores the kth largest element
-
-OR
-simply use array and sort it and keep inserting into it.
-
-*/
-
-
-
 public class KthLargest {
-    int[] queue;
+    PriorityQueue<int,int> queue;
     int k;
-    
     public KthLargest(int k, int[] nums) {
-        this.queue=new int[k];
-        for(int i=0; i<k;i++)
-            queue[i]=Int32.MinValue;
         this.k=k;
-        Array.Sort(nums);
-        Array.Reverse(nums);
+        this.queue=new PriorityQueue<int,int>();
+        foreach(int n in nums)
+            queue.Enqueue(n,n);
         
-        Array.Copy(nums,queue,Math.Min(k,nums.Length));
-        //Print();
+        while(queue.Count>k)
+            queue.Dequeue();
     }
     
-    public int Add(int val) {        
-        //Console.WriteLine("Adding:" + val);
-        int i=-1;
-        int prev;
-        while(++i<queue.Length)
-        {
-            if(queue[i]<val)
-                break;
-        }
-        //Console.WriteLine("after finding the index:"+i);
-        while(i<queue.Length)
-        {
-            int temp=queue[i];
-            queue[i++]=val;
-            val=temp;
-        }
-        
-        //Print();
-        return queue[queue.Length-1];
-    }
-    
-    public void Print()
-    {
-        Console.WriteLine(String.Join(",", queue));
+    public int Add(int val) {
+        queue.Enqueue(val,val);
+        if(queue.Count>k)
+            queue.Dequeue();
+        return queue.Peek();
     }
 }
 
@@ -60,5 +25,5 @@ public class KthLargest {
  * int param_1 = obj.Add(val);
  */
  
- //time: O(NLogN) for constructor and O(N) for Add()
- //space: O(1) for constructor and O(1) for Add()
+ //time: O(NLogN) for constructor and O(1) for Add()
+ //space: O(1) for both the constructor and Add()
