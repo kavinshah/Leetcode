1d51063eef53ec152591658e541ef78b1c70f6fc
kavinshah <38910831+kavinshah@users.noreply.github.com>
2023-09-26 08:05:47 +0530

    using the property of BST

--- problem230.cs
+++ problem230.cs
@@ -13,50 +13,37 @@
  
  
 - traverse the entire tree
-- build a max heap and insert elements
-- start with left subtree
-- if the heap is empty at the end of traversing the left subtree, then explore the right subtree
-- if not, then return the last element in the tree
-- if the tree is full while traversing the left subtree, then remove the topmost element and use the normal techniques of working with a max heap
- 
+- use the property of BST
+- 
  
  */
 
 public class Solution {
-    PriorityQueue<int, int> maxheap;
     int k;
+    int smallest;
     public int KthSmallest(TreeNode root, int k) {
-        if(root.left==null && root.right==null)
-            return root.val;
-        
-        maxheap = new PriorityQueue<int, int>();
         this.k=k;
         Traverse(root);
-        return maxheap.Peek();
+        return smallest;
     }
-    
     
     public void Traverse(TreeNode node)
     {
-        if(node==null)
+        
+        if(node==null || k<1)
             return;
         
-        if(maxheap.Count<k)
-        {
-            maxheap.Enqueue(node.val, -node.val);
-        }
-        else if(maxheap.Peek() > node.val)
-        {
-            maxheap.Dequeue();
-            maxheap.Enqueue(node.val, -node.val);
-        }
-        
-        //Console.WriteLine(maxheap.Peek());
-        
         Traverse(node.left);
+        
+        if(k-- == 1)
+        {
+            smallest=node.val;
+            return;
+        }
+        
         Traverse(node.right);
     }
 }
 
-//Time: O(Nlogk)
-//Space: O(K)
+//Time: O(K)
+//Space: O(1)
