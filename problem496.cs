e4c5d7067de152a3cdb59b913e3701c2f97fcecd
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-03-10 15:15:56 +0530

    adding problem496

--- problem496.cs
+++ problem496.cs
@@ -1,36 +1,29 @@
-/*
-
-*/
-
-
 public class Solution {
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
-        Dictionary<int,int> map = new Dictionary<int,int>();
-        Stack<int> stack = new Stack<int>();
-        int[] result=new int[nums1.Length];
+        int[] result = new int[nums1.Length];
+        Dictionary<int, int> nextGreatest = new Dictionary<int, int>();
+        Stack<int> monotonicStack = new Stack<int>();
+        int current = nums2.Length-1;
         
-        for(int i=0; i<nums2.Length; i++)
+        while(current>=0)
         {
-            while(stack.Count>0 && stack.Peek()<nums2[i])
+            while(monotonicStack.Count>0 && monotonicStack.Peek() <= nums2[current])
             {
-                int current=stack.Pop();
-                map[current]=nums2[i];
+                monotonicStack.Pop();
             }
-            stack.Push(nums2[i]);
-        }
-        
-        while(stack.Count>0)
-        {
-            map[stack.Pop()]=-1;
+            nextGreatest.Add(nums2[current], monotonicStack.Count>0?monotonicStack.Peek():-1);
+            monotonicStack.Push(nums2[current]);
+            current--;
         }
         
         for(int i=0; i<nums1.Length; i++)
         {
-            result[i]=map[nums1[i]];
+            result[i] = nextGreatest[nums1[i]];
         }
+        
         return result;
     }
 }
 
 //time: O(N)
-//space: O(N)
+//Space: O(N)
