785fa32663073973eb0f0259f51f4f3b76097d24
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-03-10 14:08:28 +0530

    Adding new simple code

--- problem232.cs
+++ problem232.cs
@@ -1,69 +1,47 @@
-/*
-
-stack1.top will contain the front of the queue
-stack1.bottom will contain the back the queue.
-
-[1]
-push 2
-[1 2]
-
-[2 1]
-
-stack2 is the temporary queue;
-
-stack1 = [1], stack2=[]
-push stack1 = [1], stack2 = [2]
-push stack1 = [1], stack2 = [2 3]
-push stack1 = [1], stack2 = [2 3 4]
-push stack1 = [1], stack2 = [2 3 4 5]
-push stack1 = [1], stack2 = [2 3 4 5 6]
-pop stack1 = [], stack2 = [2 3 4 5]
-pop stack1 = [5 4 3 2], stack2 = []
-
-
-*/
 public class MyQueue {
-    Stack<int> stack1,stack2;
+    Stack<int> stack1, stack2;
+    
     public MyQueue() {
-        stack1=new Stack<int>();
-        stack2=new Stack<int>();
+        stack1 = new Stack<int>();
+        stack2 = new Stack<int>();
     }
     
     public void Push(int x) {
-        stack2.Push(x);
+        stack1.Push(x);
     }
     
     public int Pop() {
-        if(stack1.Count==0)
+        int result=-1;
+        while(stack1.Count > 1)
         {
-            MoveElements();
+            stack2.Push(stack1.Pop());
         }
-        return stack1.Pop();
+        result = stack1.Pop();
+        while(stack2.Count > 0)
+        {
+            stack1.Push(stack2.Pop());
+        }
+        return result;
     }
     
     public int Peek() {
-        if(stack1.Count==0)
+        int result=-1;
+        while(stack1.Count > 0)
         {
-            MoveElements();
+            result = stack1.Pop();
+            stack2.Push(result);
         }
-        return stack1.Peek();
+        while(stack2.Count > 0)
+        {
+            stack1.Push(stack2.Pop());
+        }
+        return result;
     }
     
     public bool Empty() {
-        return stack1.Count==0 && stack2.Count==0;
-    }
-    
-    private void MoveElements()
-    {
-        while(stack2.Count>0)
-            stack1.Push(stack2.Pop());
+        return stack1.Count == 0;
     }
 }
-
-//Push() = O(1)
-//Pop() = O(1)
-//Peek() = O(1)
-//Empty() = O(1)
 
 /**
  * Your MyQueue object will be instantiated and called as such:
@@ -72,4 +50,18 @@ public class MyQueue {
  * int param_2 = obj.Pop();
  * int param_3 = obj.Peek();
  * bool param_4 = obj.Empty();
+ 
+ Time: 
+ Push : O(1)
+ Pop : O(N)
+ Peek : O(N)
+ Empty: O(1)
+ 
+ Space:
+ Push : O(1)
+ Pop : O(N)
+ Peek : O(N)
+ Empty: O(1)
+ 
  */
+ 
