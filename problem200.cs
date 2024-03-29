4a95d6d590f8acd62f1be12f38e942fb45acde12
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-03-29 09:50:46 +0530

    adding problem 200

--- problem200.cs
+++ problem200.cs
@@ -1,45 +1,43 @@
 public class Solution {
-    int maxRow, maxCol;
     char[][] grid;
+    int count;
     public int NumIslands(char[][] grid) {
-        int count = 0;
-        maxRow=grid.Length;
-        maxCol=grid[0].Length;
-        this.grid=grid;
-        for(int i =0; i<maxRow;i++)
+        count=0;
+        this.grid = grid;
+        
+        for(int i=0; i<grid.Length; i++)
         {
-            for(int j=0; j<maxCol; j++)
+            for(int j=0; j<grid[0].Length; j++)
             {
-                if(grid[i][j] == '1')
+                if(grid[i][j]=='1')
                 {
-                    count+=1;
+                    count++;
                     Dfs(i,j);
                 }
             }
         }
+        
         return count;
     }
     
     public void Dfs(int row, int col)
     {
-        grid[row][col]='0';
         
-        if((row+1)<maxRow && grid[row+1][col]=='1')
-        {
+        grid[row][col]='2';
+        
+        if((row+1)<grid.Length && (col)<grid[0].Length && grid[row+1][col]=='1')
             Dfs(row+1, col);
-        }
-        if((row-1)>=0 && grid[row-1][col]=='1')
-        {
-            Dfs(row-1, col);
-        }
-        if((col+1)<maxCol && grid[row][col+1]=='1')
-        {
-            Dfs(row, col+1);
-        }
-        if((col-1)>=0 && grid[row][col-1]=='1')
-        {
-            Dfs(row, col-1);
-        }
         
+        if((row)<grid.Length && (col+1)<grid[0].Length && grid[row][col+1]=='1')
+            Dfs(row, col+1);
+        
+        if((row-1)>=0 && (col)<grid[0].Length && grid[row-1][col]=='1')
+            Dfs(row-1, col);
+        
+        if((row)<grid.Length && (col-1)>=0 && grid[row][col-1]=='1')
+            Dfs(row, col-1);
     }
 }
+
+//time: O(MN)
+//space: O(MN)
