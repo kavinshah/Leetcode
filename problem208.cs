475cf94362fe7d9cd9429fc266c60f4d597e0546
kavinshah <38910831+kavinshah@users.noreply.github.com>
2024-03-31 10:13:15 +0530

    adding trie

--- problem208.cs
+++ problem208.cs
@@ -1,80 +1,60 @@
-/*
-
-apple
-
-i=0, current=a
-i=1, current=[a[p]]
-i=2, current=[a[p[p]]]
-...
-i=4, current=[a[p[p[l[e]]]]]
-
-*/
-
-
 public class Trie {
-    Node root;
+    bool isWord;
+    Dictionary<char, Trie> words;
     public Trie() {
-        root = new Node();
+        isWord = false;
+        words = new Dictionary<char, Trie>();
     }
     
     public void Insert(string word) {
-        Node current=root;
-        for(int i=0; i<word.Length; i++){
-            if(!current.map.ContainsKey(word[i]))
-                current.map[word[i]] = new Node();
-            current=current.map[word[i]];
+        
+        Trie current = this;
+        char prev;
+        foreach(char c in word)
+        {
+            if(!current.words.ContainsKey(c))
+                current.words[c] = new Trie();
+            current = current.words[c];
+            prev = c;
         }
-        current.isWord=true;
+        current.isWord = true;
     }
     
     public bool Search(string word) {
-        Node current = root;
-        return current.Search(word);
+        Trie current = this;
+        foreach(char c in word)
+        {
+            if(!current.words.ContainsKey(c))
+                return false;
+            current = current.words[c];
+        }
+        return current.isWord;
     }
     
     public bool StartsWith(string prefix) {
-        Node node = root.StartsWith(prefix);
-        if(node==null)
-            return false;
+        Trie current = this;
+        foreach(char c in prefix)
+        {
+            if(!current.words.ContainsKey(c))
+                return false;
+            current = current.words[c];
+        }
         return true;
     }
 }
 
-public class Node{
-    public Dictionary<char, Node> map;
-    public bool isWord=false;
-    public Node(){
-        map = new Dictionary<char, Node>();
-    }
-    
-    public void Print(string prefix){
-        if(isWord){
-            Console.WriteLine(prefix);
-        }
-        foreach(KeyValuePair<char, Node> kvp in map){
-            map[kvp.Key].Print(prefix+kvp.Key);
-        }
-    }
-    
-    public bool Search(string word){
-        Node node = StartsWith(word);
-        if(node==null)
-            return false;
-        return node.isWord;
-    }
-    
-    public Node StartsWith(string prefix){
-        if(prefix.Length==0)
-            return this;
-        
-        if(!map.ContainsKey(prefix[0])){
-            return null;
-        }
-        
-        return map[prefix[0]].StartsWith(prefix.Substring(1));
-    }
-}
-
+/*
+insert:
+	time: O(N)
+	space: O(1)
+search:
+	time: O(N)
+	space: O(1)
+startsWith:
+	time: O(N)
+	space: O(1)
+	
+*/
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie obj = new Trie();
@@ -82,4 +62,3 @@ public class Node{
  * bool param_2 = obj.Search(word);
  * bool param_3 = obj.StartsWith(prefix);
  */
- 
