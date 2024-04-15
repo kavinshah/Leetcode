82e6398e38abb2b69bc3813d5d1e831d7f4478d1
Kavin Shah <38910831+kavinshah@users.noreply.github.com>
2024-04-15 11:50:01 +0530

    Use constant space approach if the string data structure is mutable

--- problem151.cs
+++ problem151.cs
@@ -1,14 +1,77 @@
 public class Solution {
     public string ReverseWords(string s) {
-        Stack<string> result = new Stack<string>();
-        foreach(string word in s.Trim().Split(" "))
+        char[] input = new char[s.Length];
+        char? prev=null;
+        int inputLength = 0;
+        
+        //format the string
+        for(int i=0; i<s.Length; i++)
         {
-            if(word.Length>0)
-                result.Push(word);
+            if((prev==null && s[i]!= ' ')
+               || (s[i]>='A' && s[i]<='Z')
+               || (s[i]>='a' && s[i]<='z')
+               || (s[i]>='0' && s[i]<='9')) {
+                input[inputLength] = s[i];
+                inputLength++;
+            }
+            else if(s[i]==' ' && prev!=null && prev!=' ') {
+                input[inputLength] = s[i];
+                inputLength++;
+            }
+            prev=s[i];
         }
-        return String.Join(" ", result);
+        
+        if(input[inputLength-1]==' ')
+            inputLength=inputLength-1;
+        
+        //Console.WriteLine("{0}, {1}", String.Join("", input), inputLength);
+        
+        //reverse the string
+        for(int i=0; i<inputLength/2; i++)
+        {
+            char temp = input[inputLength-1-i];
+            input[inputLength-1-i] = input[i];
+            input[i] = temp;
+        }
+        
+        //Console.WriteLine(String.Join("", input));
+        
+        //reverse the words
+        int start=0, end;
+        for(int i=0; i<inputLength; i++)
+        {
+            if(input[i] != ' ')
+                continue;
+            
+            end=i-1;
+            
+            while(start<end)
+            {
+                char temp = input[start];
+                input[start] = input[end];
+                input[end] = temp;
+                start++;
+                end--;
+            }
+            start=i+1;
+        }
+        
+        end=inputLength-1;
+        while(start<end)
+        {
+            char temp = input[start];
+            input[start] = input[end];
+            input[end] = temp;
+            start++;
+            end--;
+        }
+        
+        return String.Join("", input.Take(inputLength));
+        
     }
 }
 
-//time: O(MN)
-//Space: O(MN)
+
+//Time: O(N)
+//Space: O(1) -- considering that the string is mutable
+//    : O(N) -- if the string is immutable
