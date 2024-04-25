9dd2c2a092fc6d66afb3990bf564ba83ef514241
Kavin <38910831+kavinshah@users.noreply.github.com>
2024-04-25 11:44:56 +0530

    Add problem 5

--- problem5.cs
+++ problem5.cs
@@ -1,53 +1,41 @@
 public class Solution {
-    int result=0;
-    string longestString="";
-    string s;
+    string input;
+    int longestSubstringLength = 0;
+    string longestSubstring="";
     
     public string LongestPalindrome(string s) {
-        this.s=s;
+        input = s;
+        
         for(int i=0; i<s.Length; i++)
         {
-            for(int j=i+result; j<s.Length; j++)
+            for(int j=s.Length-i; j>longestSubstringLength; j--)
             {
-                if(IsPalinrome(i,j) && (j-i+1)>result)
+                if(CheckPalindrome(i, j))
                 {
-                    longestString=s.Substring(i,j-i+1);
-                    result=j-i+1;
+                    longestSubstringLength = Math.Max(longestSubstringLength, j);
+                    longestSubstring = input.Substring(i, j);
+                    break;
                 }
             }
         }
-        return longestString;
+        return longestSubstring;
     }
-    
-    public bool IsPalinrome(int start, int end)
+
+    public bool CheckPalindrome(int startIndex, int length)
     {
-        while(start<=end)
+        int endIndex = startIndex+length-1;
+        
+        while(startIndex<=endIndex)
         {
-            if(s[start]!=s[end])
+            if(input[startIndex]!=input[endIndex])
                 return false;
-            start++;
-            end--;
+            startIndex++;
+            endIndex--;
         }
         return true;
     }
+
 }
 
-// len=5.
-// eg. aaaaa
-//i=0, total=5, 1+2+3+4+5
-//i=1, total=0
-//i=2, total=0
-//i=3, total=0
-//i=4, total=0
-
-//eg=abcde
-// i=0, total=5. 1+1+1+1+1
-// i=1, total=4. 1+1+1+1
-// i=2, total=3. 1+1+1
-// i=3, total=2. 1+1
-// i=4, total=1. 1
-
-//I think the time complexity is O(N^2) but looking at the code there are 3 loops and it leads to O(N^3)
-
-//Time: O(N^2)
+//time: O(N^2*K) -- N=length of input, K=length of longest palindrome
 //space: O(1)
