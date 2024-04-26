18ba08cac00c3437dfa01fd5aa2454951c67c030
Kavin <38910831+kavinshah@users.noreply.github.com>
2024-04-26 10:55:02 +0530

    Add problem 28 with KMP algorithm

--- problem28.cs
+++ problem28.cs
@@ -4,28 +4,55 @@ public class Solution {
     public int StrStr(string haystack, string needle) {
         this.needle=needle;
         this.haystack = haystack;
-        
-        for(int i=0; i<=haystack.Length-needle.Length; i++)
-        {
-            if(CheckNeedleInStack(i))
-                return i;
-        }
-        
-        return -1;
+        int[] lps = ComputeLps(needle);
+        //Console.WriteLine(String.Join(",", lps));
+        //return -1;
+        return FindNeedleInHaystack(haystack, needle, lps);
     }
     
-    public bool CheckNeedleInStack(int start)
+    public int[] ComputeLps(string needle)
     {
-        int index=0;
-        while(index<needle.Length)
-        {
-            if(haystack[index+start]!=needle[index])
-                return false;
-            index++;
+        int[] lps = new int[needle.Length];
+        int index=1;
+        int previousLps = 0;
+        
+        while(index<needle.Length) {
+            if(needle[previousLps]==needle[index]) {
+                previousLps+=1;
+                lps[index]=previousLps;
+                index++;
+            } else if(previousLps==0){
+                lps[index]=0;
+                index++;
+            } else{
+                previousLps = lps[previousLps-1];
+            }
         }
-        return true;
+        return lps;
     }
+    
+    public int FindNeedleInHaystack(string haystack, string needle, int[] lps)
+    {
+        int i=0;
+        int j=0;
+        
+        while(i<haystack.Length && j<needle.Length){
+            
+            if(haystack[i]==needle[j]){
+                i++;
+                j++;
+            } else if(j==0) {
+                i+=1;
+            } else {
+                j = lps[j-1];
+            }
+        }
+        
+        if(j==needle.Length)
+            return i-needle.Length;
+        return -1;
+    } 
 }
 
-//time: O(MN)
-//space: O(1)
+//Time: O(M+N)
+//Space: O(1)
