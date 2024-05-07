789b983b43ab063da77cedeb440b17e63f508f9d
Kavin <38910831+kavinshah@users.noreply.github.com>
2024-05-07 10:10:41 +0530

    Use dp and memoization

--- 0_1_Knapsack.cs
+++ 0_1_Knapsack.cs
@@ -50,7 +50,6 @@ w=4
 v = 1   2   3
 w = 4   5   1
 
-
 v/w
 
 1   2   4   5
@@ -63,7 +62,6 @@ w=5
 w = 1   2   4   5
 v = 5   4   8   6
 
-
                             0,0,0
                         /     |     \
                     1,1,5   2,2,4   3,4,8
@@ -72,39 +70,62 @@ v = 5   4   8   6
         /   |   \   
     x       x   x
 
+    1   2   4   5
+5   
+4
+8
+6
+
+
+
 */
 
 class Solution
     {
         //Complete this function
-        int maxValue;
+        int[][] dp;
         int[] wt, val;
-        int W;
         public int knapSack(int W, int[] wt, int[] val,int n)
         {
             //Your code here
             this.wt=wt;
             this.val=val;
-            this.W = W;
-            FindMaxValue(0, 0, 0);
-            return maxValue;
+            this.dp = new int[n][];
+            
+            for(int i=0; i<n;i++){
+                this.dp[i]=new int[W+1];
+                for(int j=0; j<W+1; j++)
+                    dp[i][j]=-1;
+            }
+            
+            FindMaxValue(n-1, W);
+            
+            // for(int i=0; i<n; i++)
+            //     Console.WriteLine(String.Join(",", dp[i]));
+                
+            return dp[n-1][W];
         }
         
-        public void FindMaxValue(int index, int currentWeight, int currentValue){
-            if(index>=val.Length){
-                return;
+        public int FindMaxValue(int index, int currentWeight){
+            if(index==0){
+                if(currentWeight>=wt[0])
+                    dp[index][currentWeight] = val[0];
+                else
+                    dp[index][currentWeight] = 0;
+                return dp[index][currentWeight];
             }
             
-            for(int i=index; i<wt.Length; i++){
-                if((currentWeight+wt[i])>W){
-                    continue;
-                }
-                
-                maxValue=Math.Max(maxValue, currentValue+val[i]);
-                FindMaxValue(i+1, currentWeight+wt[i], currentValue+val[i]);
-            }
+            if(dp[index][currentWeight] != -1)
+                return dp[index][currentWeight];
             
-            return;
-        } 
-        
+            int taken = 0;
+            int notTaken=FindMaxValue(index-1, currentWeight);
+            if(currentWeight>=wt[index])
+                taken = val[index] + FindMaxValue(index-1, currentWeight-wt[index]);
+            dp[index][currentWeight] = Math.Max(taken, notTaken);
+            return dp[index][currentWeight];
+        }
     }
+	
+//time : O(NW)
+//Space: O(NW)
