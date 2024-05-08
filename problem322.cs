1e30a6fa690dc9e415755dbbc329c99b2075b7b0
Kavin <38910831+kavinshah@users.noreply.github.com>
2024-05-08 08:50:29 +0530

    Add problem 322

--- problem322.cs
+++ problem322.cs
@@ -1,24 +1,46 @@
-public class Solution {
-    public int CoinChange(int[] coins, int V) {
-        uint[] table = Enumerable.Repeat(UInt32.MaxValue, V+1).ToArray();
-        int M = coins.Length;
-        
-        table[0]=0;
+ /*
 
-        for(int i=1; i<=V; i++){
-            for(int j=0; j<M; j++){
-                if(i>=coins[j] && table[i-coins[j]]!=UInt32.MaxValue){
-                    table[i] = Math.Min(table[i], 1+table[i-coins[j]]);
+ 0 1 2
+[5,2,1]
+
+
+                            11
+                    /       |
+                    6     
+            /       |       \
+            1       4       5
+    /   |  \    /   |   \
+    x   x  0    x   2    3
+            |
+            3
+
+
+
+    
+*/
+
+public class Solution {
+    int[] dp;
+    int[] coins;
+    public int CoinChange(int[] coins, int amount) {
+        this.coins = coins;
+        this.dp = Enumerable.Repeat(Int32.MaxValue, amount+1).ToArray();
+        dp[0]=0;
+        
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<coins.Length; j++){
+                if(i>=coins[j] && dp[i-coins[j]]!=Int32.MaxValue){
+                    dp[i] = Math.Min(dp[i], dp[i-coins[j]]+1);
                 }
             }
         }
-
-        if(table[V]==UInt32.MaxValue)
+        
+        if(dp[amount]==Int32.MaxValue)
             return -1;
         
-        return (int)table[V];
+        return dp[amount];
     }
 }
 
-//time: O(M*V)
-//space: O(V)
+//time: O(N*Amount)
+//Space: O(Amount)
