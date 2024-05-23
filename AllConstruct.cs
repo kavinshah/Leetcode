771d260a8b82d0548315fba47e76fe8700badf8b
Kavin <38910831+kavinshah@users.noreply.github.com>
2024-05-23 11:10:43 +0530

    Revisit DP problems

---
+++ AllConstruct.cs
@@ -0,0 +1,8 @@
+/*
+
+Given a target and an array of strings - wordbank, return all the ways in which the target string can be constructed using elements from the wordbank.
+
+We can reuse elements of wordbank any number of times.
+
+*/
+
---
+++ CanConstruct.cs
@@ -0,0 +1,8 @@
+/*
+
+Given a target and an array of strings - wordbank, return a boolean if the target string can be constructed from the wordbank.
+
+We can reuse elements of wordbank any number of times.
+
+*/
+
